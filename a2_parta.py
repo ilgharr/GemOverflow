2048504dba9303be1c77c2f8c2e667fece342113
Sheida Hashem Dabbaghian <163951625+SheidaDBG@users.noreply.github.com>
2024-11-28 10:36:03 -0500

    Update a2_parta.py

--- a2_parta.py
+++ a2_parta.py
@@ -2,12 +2,19 @@
 #    Main Reviewer(s):
 
 class HashTable:
+	class Node:
+		def __init__(self, key, value):
+			self.key = key
+			self.value = value
+			self.next = None
 
 	# You cannot change the function prototypes below.  Other than that
 	# how you implement the class is your choice as long as it is a hash table
 
 	def __init__(self, cap=32):
-		pass
+		self._capacity = cap
+		self.table = [None] * self._capacity
+		self.size = 0
 
 	def insert(self,key, value):
 		pass
